efca5adc4bac5bbfae3ca973161c846434d06864
Cody Parman <crparmasean@gmail.com>
2024-06-11 23:28:30 -0700

    Completed project 4

--- HDL/projects/4/fill/Fill.asm
+++ HDL/projects/4/fill/Fill.asm
@@ -8,4 +8,47 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+(START)
+    @SCREEN // First address of memory block reserved for screen, which is 256x512 pixels, stored in 8k of data. For each pixel, 0=white and 1=black
+    D=A
+    @i // Stores memory value of current screen pixel
+    M=D
+
+(INPUT)
+    @KBD // memory address reserved for keyboard, which immediately follows the block of memory reserved for the screen
+    D=M
+    @BLACK
+    D;JGT // If there was any keyboard input, set pixel color register to black
+    @WHITE
+    D;JEQ // If there was no keyboard input, set pixel color register to white
+
+(BLACK) // Sets color register to black (all ones)
+    @color
+    M=-1
+    @RECOLOR
+    0;JMP
+
+(WHITE) // Sets color register to white (all zeros)
+    @color
+    M=0
+    @RECOLOR
+    0;JMP
+
+(RECOLOR)
+    // Color current pixel
+    @color
+    D=M
+    @i
+    A=M
+    M=D
+    
+    // Iterate to next pixel, unless we've passed the last screen memory address and reached @KBD
+    @KBD
+    D=A
+    @i
+    M=M+1
+    D=D-M
+    @START
+    D;JEQ // Restart program if we've reached the end of the screen memory
+    @RECOLOR
+    0;JMP // Otherwise, keep coloring pixels
--- HDL/projects/4/mult/Mult.asm
+++ HDL/projects/4/mult/Mult.asm
@@ -7,4 +7,60 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+// Uses successive addition and bit-shift operations to calcualte R0*R1
+// Ignores leftmost bit since program is meant for strictly positive inputs only
+// Bit shifts start from the most significant bit of R1
+// R0 is added to R2 if R1&(2^15) is positive
+// R2 is then doubled whenever the loop restarts and checks the next bit of R1
+
+// Initialize result as 0
+@R2
+M=0
+
+// Store the current bit of R1 we are checking in R3
+@15
+D=A
+@R3
+M=D
+
+// Calculate and store 2^15=32768 in R4 by doubling 2^14=16384, because we can't @32768 directly (out of memory range allowed by compiler)
+@16384
+D=A
+D=D+A
+@R4
+M=D
+
+
+(LOOP)
+    // Left-shift R1 and R2
+    @R1
+    D=M
+    M=D+M
+    @R2
+    D=M
+    M=D+M
+
+    // Check what is now 16th bit of R1, if it's 1 then add R0 to R2
+    @R4
+    D=M
+    @R1
+    D=D&M
+    @CHCK
+    D;JEQ // Skip addition step if 15th bit was 0
+    
+    // Add R0 to R2
+    @R0
+    D=M
+    @R2
+    M=D+M
+
+    (CHCK)
+    // Decrement R3, then jump back to start of loop if result is greater than 0
+    @R3
+    MD=M-1
+    @LOOP
+    D;JGT
+
+(END)
+@END
+0;JMP
